979c1675c0cfb6fcf8080098839309639dcf11ef
Hariel <hariel@hotmail.com>
2023-03-09 09:59:12 -0300

    initial commit

---
+++ src/main/java/com/cluster8/c8/C8Application.java
@@ -0,0 +1,13 @@
+package com.cluster8.c8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cluster8/c8/C8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cluster8.c8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
