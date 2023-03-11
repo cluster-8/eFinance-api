1ed1e32ec8fe0543f4f83f2457af04af3ed7de55
Hariel <hariel@hotmail.com>
2023-03-11 11:17:36 -0300

    feat: pg connection

---
+++ src/main/java/com/cluster8/c8/config/Database.java
@@ -0,0 +1,30 @@
+package com.cluster8.c8.config;
+
+import com.zaxxer.hikari.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import javax.sql.DataSource;
+
+@Configuration
+public class Database {
+
+  @Value("${spring.datasource.url}")
+  private String dbUrl;
+
+  @Value("${spring.datasource.username}")
+  private String username;
+
+  @Value("${spring.datasource.password}")
+  private String password;
+
+  @Bean
+  public DataSource dataSource() {
+    HikariConfig config = new HikariConfig();
+
+    config.setJdbcUrl(dbUrl);
+    config.setUsername(username);
+    config.setPassword(password);
+
+    return new HikariDataSource(config);
+  }
+}
