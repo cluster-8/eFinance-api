add42b79eaf3a269eae8809318fcf4ba8b636254
Hariel <hariel@hotmail.com>
2023-03-14 07:45:07 -0300

    feat: ddl.sql

--- DDL.sql
+++ DDL.sql
@@ -1,50 +1,43 @@
+-- drop table if EXISTS tarifas;
+-- drop table if EXISTS servicos;
+-- drop table if EXISTS instituicoes;
+-- drop table if EXISTS grupo;
+
 -- CRIANDO TABELA DE GRUPOS CONSOLIDADOS
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-create table if not exists gc_grupo (
-  gc_id uuid DEFAULT uuid_generate_v4 (),
-  gc_codigo varchar(4) not null,
-  gc_nome varchar(150) not null,
-  primary key (gc_id),
-  unique(gc_codigo)
-);
-
 -- CRIANDO TABELA DE INSTITUIÇÕES FINANCEIRAS
-create table if not exists if_instituicao (
-	if_id uuid DEFAULT uuid_generate_v4 (),
-	if_grupo_id uuid null,
-	if_nome varchar(100) not null,
-	if_cnpj varchar(14) not null,
-	if_cnpj_formatado varchar(14),
-	primary key (if_id),
-	CONSTRAINT fk_grupo
-      FOREIGN KEY(if_grupo_id) 
-	  REFERENCES gc_grupo(gc_id)
+create table if not exists instituicoes (
+	"id" uuid primary key DEFAULT uuid_generate_v4 (),
+	"nome" varchar(100) not null,
+	"cnpj" varchar(14) not null,
+	"cnpjFormatado" varchar(14),
+
+	unique ("nome", "cnpj")
 );
 
 -- CRIANDO TABELA DE SERVIÇOS
-create table if not exists sp_servico (
-	sp_id uuid default uuid_generate_v4 (),
-	sp_codigo varchar(4) not null,
-	sp_tipo char(1) not null,
-	primary key (sp_id)
+create table if not exists servicos (
+	"id" uuid primary key default uuid_generate_v4 (),
+	"codigo" varchar(4) not null,
+	"tipo" char(1) not null,
+
+	unique ("codigo", "tipo")
 );
 
 -- CRIANDO TABELA DE TARIFAS
-create table if not exists ts_tarifa (
-	ts_id uuid default uuid_generate_v4 (),
-	ts_servico_id uuid not null,
-	ts_instituicao_id uuid not null,
-	ts_valor_maximo decimal(8,2) not null,
-	ts_data_vigencia date not null,
-	ts_unidade varchar(20),
-	ts_periodicidade varchar(20),
-	ts_moeda varchar(10),
-	primary key (ts_id),
-	CONSTRAINT fk_servico
-      FOREIGN KEY(ts_servico_id)
-	  REFERENCES sp_servico(sp_id),
-	CONSTRAINT fk_instituicao
-      FOREIGN KEY(ts_instituicao_id)
-	  REFERENCES if_instituicao(if_id)
+create table if not exists tarifas (
+	"id" uuid primary key default uuid_generate_v4 (),
+	"servicoId" uuid not null,
+	"instituicaoId" uuid not null,
+	"valorMaximo" float not null,
+	"dataVigencia" date not null,
+	"unidade" varchar(20),
+	"periodicidade" varchar(20),
+	"moeda" varchar(10),
+
+	constraint fk_servico foreign key("servicoId") references servicos(id),
+	constraint fk_instituicao foreign key("instituicaoId") references instituicoes(id),
+
+	unique ("servicoId", "instituicaoId", "valorMaximo", "dataVigencia", "periodicidade", "unidade")
 );
