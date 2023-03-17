bdf3496ca4670677dba42eb10f48fca0e37141ec
Hariel <hariel@hotmail.com>
2023-03-17 08:14:51 -0300

    feat: C8Applicaton.java - enable caching

--- src/main/java/com/cluster8/c8/C8Application.java
+++ src/main/java/com/cluster8/c8/C8Application.java
@@ -2,7 +2,9 @@ package com.cluster8.c8;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class C8Application {
 
