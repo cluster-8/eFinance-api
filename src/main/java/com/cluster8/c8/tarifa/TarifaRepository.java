98757abb2e2acdc7683fe5852b128d2c9f0307ab
vinicius-hso <viniciushso@icloud.com>
2023-04-12 10:04:34 -0300

    Feat: add tariff minimum value

--- src/main/java/com/cluster8/c8/tarifa/TarifaRepository.java
+++ src/main/java/com/cluster8/c8/tarifa/TarifaRepository.java
@@ -14,10 +14,10 @@ import com.cluster8.c8.tarifa.dto.FindAllTarifasByInstituicaoDto;
 public interface TarifaRepository extends JpaRepository<TarifaEntity, UUID> {
   Optional<TarifaEntity> findById(UUID id);
 
-  @Query("select new com.cluster8.c8.tarifa.dto.FindAllTarifasByInstituicaoDto(t.id, t.moeda, t.unidade, t.periodicidade, t.valorMaximo, t.dataVigencia, s.id, s.nome) from TarifaEntity as t join ServicoEntity as s on s.id = t.servico where t.instituicao.id=:instituicaoId")
+  @Query("select new com.cluster8.c8.tarifa.dto.FindAllTarifasByInstituicaoDto(t.id, t.moeda, t.unidade, t.periodicidade, t.valorMaximo, t.valorMinimo, t.dataVigencia, s.id, s.nome) from TarifaEntity as t join ServicoEntity as s on s.id = t.servico where t.instituicao.id=:instituicaoId")
   List<FindAllTarifasByInstituicaoDto> findByInstituicaoId(UUID instituicaoId);
 
-  @Query("select new com.cluster8.c8.tarifa.dto.FindAllTarifasByInstituicaoDto(t.id, t.moeda, t.unidade, t.periodicidade, t.valorMaximo, t.dataVigencia, s.id, s.nome) from TarifaEntity as t join ServicoEntity as s on s.id = t.servico where t.instituicao.id=:instituicaoId and s.tipo=:tipo")
+  @Query("select new com.cluster8.c8.tarifa.dto.FindAllTarifasByInstituicaoDto(t.id, t.moeda, t.unidade, t.periodicidade, t.valorMaximo, t.valorMinimo, t.dataVigencia, s.id, s.nome) from TarifaEntity as t join ServicoEntity as s on s.id = t.servico where t.instituicao.id=:instituicaoId and s.tipo=:tipo")
   List<FindAllTarifasByInstituicaoDto> findByInstituicaoIdAndServicoTipo(UUID instituicaoId, String tipo);
 
 }
