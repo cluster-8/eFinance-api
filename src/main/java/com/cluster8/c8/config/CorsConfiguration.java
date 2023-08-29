2212591f3f2f789beca519ab635ae2360ce7ab10
vinicius-hso <viniciushso@icloud.com>
2023-08-29 18:31:53 -0300

    Fix: allowed origins

--- src/main/java/com/cluster8/c8/config/CorsConfiguration.java
+++ src/main/java/com/cluster8/c8/config/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .exposedHeaders("*")
-        .allowedOrigins("http://35.238.2.146:8080")
+        .allowedOrigins("http://35.238.2.146:8080", "https://efinance-front-w4v2grm6xa-uc.a.run.app")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
   }
 }
