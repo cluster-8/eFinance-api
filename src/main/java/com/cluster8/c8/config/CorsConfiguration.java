ab9ec821d6af775bd2c0d0196e8dff3bf060232c
vinicius-hso <viniciushso@icloud.com>
2023-08-30 17:25:09 -0300

    fix: allowed origins

--- src/main/java/com/cluster8/c8/config/CorsConfiguration.java
+++ src/main/java/com/cluster8/c8/config/CorsConfiguration.java
@@ -11,7 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .exposedHeaders("*")
-        .allowedOrigins("https://cosmic-syrniki-499c25.netlify.app")
+        .allowedOrigins("https://gcp-deploy--cosmic-syrniki-499c25.netlify.app")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
   }
 }
